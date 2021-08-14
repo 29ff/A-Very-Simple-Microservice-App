ff298669107648565185ef17c7fd5d525a74977b
Hung Nguyen Ba <nbhung189@gmail.com>
2021-08-14 16:12:02 +0700

    update moderation service for comment filter

---
+++ moderation/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/events", (req, res) => {});
+
+app.listen(4003, () => {
+  console.log("Moderation service is listening on port 4003");
+});
